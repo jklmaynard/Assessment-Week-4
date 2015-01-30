6539a2143cfd084537ccd3a23fc1f927dc259046
James Maynard <jameskirkmaynard@gmail.com>
2015-01-30 08:59:36 -0800

    Create database and write first migration.

---
+++ db/migrate/20150130165504_create_tables_for_database.rb
@@ -0,0 +1,17 @@
+class CreateTablesForDatabase < ActiveRecord::Migration
+  def change
+    create_table(:bands) do |t|
+      t.column(:name, :string)
+    end
+
+    create_table(:venues) do |t|
+      t.column(:name, :string)
+    end
+
+    create_table(:bands_venues) do |t|
+      t.column(:band_id, :integer)
+      t.column(:venue_id, :integer)
+    end
+
+  end
+end
