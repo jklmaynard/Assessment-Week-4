8e209574bf24e7d99cbde6435984f16f37f213eb
James Maynard <jameskirkmaynard@gmail.com>
2015-01-30 10:18:20 -0800

    Add band post to index.erb, write redirects.

--- app.rb
+++ app.rb
@@ -3,13 +3,22 @@ Bundler.require(:default)
 
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
+# INDEX.erb CRUD
+
 get("/") do
   @venues = Venue.all()
+  @bands = Band.all()
   erb(:index)
 end
 
 post("/venue") do
   venue = Venue.create({ :name => params["name"], :location => params["location"] })
   @venues = Venue.all()
-  erb(:index)
+  redirect("/")
+end
+
+post("/band") do
+  band = Band.create({ :name => params["name"] })
+  @bands = Band.all()
+  redirect("/")
 end
