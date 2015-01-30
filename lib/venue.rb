bc2640513c6972dd6aa6ec8d15e7da78be33c47a
James Maynard <jameskirkmaynard@gmail.com>
2015-01-30 12:21:03 -0800

    Add validations to :name, :location keys in Venue class.

--- lib/venue.rb
+++ lib/venue.rb
@@ -1,3 +1,5 @@
 class Venue < ActiveRecord::Base
   belongs_to(:band)
+  validates(:name, :presence => true)
+  validates(:location, :presence => true)
 end
--- spec/venue_spec.rb
+++ spec/venue_spec.rb
@@ -2,4 +2,14 @@ require("spec_buddy")
 
 describe(Venue) do
   it { should belong_to(:band) }
+
+  it("validates the presence of a name") do
+    venue = Venue.new({:name => ""})
+    expect(venue.save()).to eq(false)
+  end
+
+  it("validates the presence of a location") do
+    venue = Venue.new({:location => ""})
+    expect(venue.save()).to eq(false)
+  end
 end
