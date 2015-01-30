68975036966eda7e57dd97cef44ad59161804b4e
James Maynard <jameskirkmaynard@gmail.com>
2015-01-30 12:17:20 -0800

    Add validation to :name key in band class.

--- lib/band.rb
+++ lib/band.rb
@@ -1,3 +1,4 @@
 class Band < ActiveRecord::Base
   has_many(:venues)
+  validates(:name, :presence => true)
 end
--- spec/band_spec.rb
+++ spec/band_spec.rb
@@ -2,4 +2,9 @@ require("spec_buddy")
 
 describe(Band) do
   it { should have_many(:venues) }
+
+  it("validates the presence of a name") do
+    band = Band.new({:name => ""})
+    expect(band.save()).to eq(false)
+  end
 end
