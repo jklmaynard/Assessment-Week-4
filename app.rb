48c35593c99a0e25c0be0b046104d216eeaaee6b
James Maynard <jameskirkmaynard@gmail.com>
2015-01-30 09:59:43 -0800

    Add venue post to index.erb.

--- app.rb
+++ app.rb
@@ -4,5 +4,12 @@ Bundler.require(:default)
 Dir[File.dirname(__FILE__) + '/lib/*.rb'].each { |file| require file }
 
 get("/") do
+  @venues = Venue.all()
+  erb(:index)
+end
+
+post("/venue") do
+  venue = Venue.create({ :name => params["name"], :location => params["location"] })
+  @venues = Venue.all()
   erb(:index)
 end
